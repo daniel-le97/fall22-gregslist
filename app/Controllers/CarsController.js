129678d0eb478bd9ce9ad289dbbfd388271bd4aa
daniel-le97 <daniel97.le@gmail.com>
2022-09-07 21:52:04 -0600

    got forms to work atleast

--- app/Controllers/CarsController.js
+++ app/Controllers/CarsController.js
@@ -9,14 +9,54 @@ function drawCars() {
   appState.cars.forEach((car) => (template += car.CarCardTemplate));
   // TODO trigger bad set
   setHTML("listings", template);
-  // _drawCarsForm();
+  _drawCarsForm();
 }
 
-// function _drawCarsForm() {
-//   let template = "";
+function _drawCarsForm() {
+  let template = "";
+  template += /*html*/ `
+  <form onsubmit="app.carsController.addCar()">
+        <div class="form-floating mb-3">
+          <input type="text" class="form-control" name="make" required minlength="3" maxlength="20">
+          <label for="make">Make</label>
+        </div>
 
-//   document.getElementById("formDiv").innerHTML = template;
-// }
+        <div class="form-floating mb-3">
+          <input type="text" class="form-control" name="model" required>
+          <label for="model">Model</label>
+        </div>
+
+        <div class="form-floating mb-3">
+          <input type="number" class="form-control" name="year" required min="1886" max="9999">
+          <label for="year">Year</label>
+        </div>
+
+        <div class="form-floating mb-3">
+          <input type="number" class="form-control" name="price" required min="0">
+          <label for="price">Price</label>
+        </div>
+
+        <div class="form-floating mb-3">
+          <input type="url" class="form-control" name="imgUrl">
+          <label for="imgUrl">Image Url <i>(We are too lazy for uploads)</i></label>
+        </div>
+
+        <div class="form-floating">
+          <textarea class="form-control" placeholder="Describe your Listing" name="description"></textarea>
+          <label for="description">Description</label>
+        </div>
+
+        <div class="d-flex my-4 gap-5 align-items-center">
+          <button class="btn" type="reset">Cancel</button>
+          <button class="btn btn-primary" type="submit">Submit</button>
+        </div>
+      </form>
+  `;
+  document.getElementById("formDiv").innerHTML = template;
+
+  document.getElementById("addListing").innerText = "🚗 Add Car";
+}
+function _drawButton() {}
 
 export class CarsController {
   constructor() {
--- app/Controllers/JobsController.js
+++ app/Controllers/JobsController.js
@@ -1,5 +1,7 @@
 import { appState } from "../AppState.js";
 import { Job } from "../Models/Job.js";
+import { jobsService } from "../Services/JobsService.js";
+import { getFormData } from "../Utils/FormHandler.js";
 import { setHTML } from "../Utils/Writer.js";
 
 function drawJobs() {
@@ -7,12 +9,52 @@ function drawJobs() {
   appState.jobs.forEach((job) => (template += job.JobListingTemplate));
 
   setHTML("listings", template);
-//   // _drawJobsForm();
+  _drawJobsForm();
 }
-// function _drawJobsForm() {
+function _drawJobsForm() {
+  let template = "";
+  template += `
   
-//   setHTML("formDiv", appState.jobs);
-// }
+  <form onsubmit="app.jobsController.addJob()">
+        <div class="form-floating mb-3">
+          <input type="text" class="form-control" name="job" required minlength="3" maxlength="20">
+          <label for="job">Job</label>
+        </div>
+
+        <div class="form-floating mb-3">
+          <input type="text" class="form-control" name="company" required>
+          <label for="company">Company</label>
+        </div>
+
+        <div class="form-floating mb-3">
+          <input type="number" class="form-control" name="hours" required min="1886" max="9999">
+          <label for="hours">Hours</label>
+        </div>
+
+        <div class="form-floating mb-3">
+          <input type="number" class="form-control" name="salary" required min="0">
+          <label for="salary">Salary</label>
+        </div>
+
+        <div class="form-floating mb-3">
+          <input type="url" class="form-control" name="imgUrl">
+          <label for="imgUrl">Image Url <i>(We are too lazy for uploads)</i></label>
+        </div>
+
+        <div class="form-floating">
+          <textarea class="form-control" placeholder="Describe your Listing" name="description"></textarea>
+          <label for="description">Description</label>
+        </div>
+
+        <div class="d-flex my-4 gap-5 align-items-center">
+          <button class="btn" type="reset">Cancel</button>
+          <button class="btn btn-primary" type="submit">Submit</button>
+        </div>
+      </form>`;
+  setHTML("formDiv", template);
+
+  document.getElementById("addListing").innerText = "🏬 Add Job";
+}
 
 export class JobsController {
   constructor() {
@@ -23,5 +65,15 @@ export class JobsController {
     drawJobs();
   }
 
-  addJobs() {}
+  addJobs() {
+    try {
+      window.event.preventDefault();
+      const form = window.event.target;
+      let formData = getFormData(form);
+      jobsService.addJob(formData);
+      drawJobs();
+    } catch (error) {
+      console.log("addJob", error);
+    }
+  }
 }
